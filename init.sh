df92fe883aef0abbd728bdb340e760489d50a6b8
dorroddorrod <dormullor@gmail.com>
2022-04-26 07:03:41 +0300

    Update

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ helm install argo-cd . -f ../values.yaml -f values.yaml --namespace $argoNamespa
 cd ../../../
 kubectl apply -f environments/templates/argocd-project.yaml
 kubectl apply -f environments/templates/app-of-apps.yaml
-sleep 30
+sleep 60
 kubectl -n $argoNamespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 kubectl port-forward service/argocd-server -n $argoNamespace 8080:443
