0d81f348b0b7374eb53acc131f997e5a7dd4db7a
dorroddorrod <dormullor@gmail.com>
2022-04-26 06:43:44 +0300

    Update

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-helm install argocd argo/argo-cd
+export envName=dev-main-01
+helm install argocd applications/argocd/$envName -f applications/argocd/$envName/$envName-values.yaml
 kubectl apply -f environments/templates/argocd-project.yaml
 kubectl apply -f environments/templates/app-of-apps.yaml
