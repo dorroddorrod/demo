6a6926e102fc80e459a5868644f1323ca429b0de
dorroddorrod <dormullor@gmail.com>
2022-04-26 07:30:22 +0300

    Update

--- init.sh
+++ init.sh
@@ -5,8 +5,42 @@ cd applications/argo-cd/$envName
 helm dep up
 helm install argo-cd . -f ../values.yaml -f values.yaml --namespace $argoNamespace --create-namespace
 cd ../../../
-kubectl -n $argoNamespace apply -f environments/templates/argocd-project.yaml
-kubectl -n $argoNamespace apply -f environments/templates/app-of-apps.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: argocd
+spec:
+  description: project for management of Argo CD itself
+  clusterResourceWhitelist:
+  - group: '*'
+    kind: '*'
+  destinations:
+  - namespace: argo-cd
+    server: https://kubernetes.default.svc
+  sourceRepos:
+  - https://github.com/dorroddorrod/demo.git
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app-of-apps
+spec:
+  destination:
+    namespace: argo-cd
+    server: https://kubernetes.default.svc
+  project: argocd
+  syncPolicy:
+    automated:
+      prune: true
+  source:
+    path: environments
+    repoURL: https://github.com/dorroddorrod/demo.git
+    targetRevision: HEAD
+    helm:
+      valueFiles:
+      - $envName.yaml
+EOF
 sleep 60
 kubectl -n $argoNamespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 kubectl port-forward service/argo-cd-argocd-server -n $argoNamespace 8080:443
